0e637c36cf3f760fab053f3ea2ff810c0b8787c7
Tingshen Yan <sunnyyants@gmail.com>
2014-10-05 19:30:41 -0700

    add in-line update function in search result list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
 
   before_action :get_friendships, only: [:show]
 
+  before_action :return_current_user, only: [:index]
+
   before_filter :require_login, only: [:update, :edit,:index,:update]
 
   def new
@@ -27,7 +29,7 @@ class UsersController < ApplicationController
 
   def index
     if params[:search]
-      @users = User.where('name LIKE?', "%#{params[:search]}%").paginate(:per_page => 2, :page => params[:page])
+      @users = User.where('name LIKE?', "%#{params[:search]}%").paginate(:per_page => 3, :page => params[:page])
     else
       @users = []
     end
@@ -48,6 +50,10 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def return_current_user
+    @user = current_user
   end
 
   def user_params
